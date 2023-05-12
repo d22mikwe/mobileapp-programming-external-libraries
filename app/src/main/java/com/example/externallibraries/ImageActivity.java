0ca0881f68a0f84f6679f359afaf68f06094a1b3
Micke <micke.welander@live.se>
2023-05-12 16:04:52 +0200

    Changed external library, added different dependancies

--- app/src/main/java/com/example/externallibraries/ImageActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.externallibraries;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class ImageActivity extends AppCompatActivity{
-}
--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -2,67 +2,30 @@ package com.example.externallibraries;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.module.AppGlideModule;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private Intent intent;
-    private Button buttonTopLeft;
-    private Button buttonTopRight;
-    private Button buttonBottomLeft;
-    private Button buttonBottomRight;
+    ImageView imgV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intent = new Intent(MainActivity.this, ImageActivity.class);
+        imgV = findViewById(R.id.imageView);
 
-        buttonBottomLeft = (Button) findViewById(R.id.buttonBottomLeft);
-        buttonBottomRight = (Button) findViewById(R.id.buttonBottomRight);
-        buttonTopLeft = (Button) findViewById(R.id.buttonTopLeft);
-        buttonTopRight = (Button) findViewById(R.id.buttonTopRight);
+        Glide.with(this)
+                .load("https://media3.giphy.com/media/VgxzQM2GYuKQXJgquJ/giphy.gif")
+                .fitCenter()
+                .into(imgV);
 
-        buttonBottomLeft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Bundle extras1 = new Bundle();
-                extras1.putString("URL", "");
-                intent.putExtras(extras1);
-                startActivity(intent);
-            }
-        });
 
-        buttonBottomRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Bundle extras2 = new Bundle();
-                extras2.putString("URL", "");
-                intent.putExtras(extras2);
-                startActivity(intent);
-            }
-        });
-
-        buttonTopLeft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Bundle extras3 = new Bundle();
-                extras3.putString("URL", "");
-                intent.putExtras(extras3);
-                startActivity(intent);
-            }
-        });
-
-        buttonTopRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Bundle extras4 = new Bundle();
-                extras4.putString("URL", "");
-                intent.putExtras(extras4);
-                startActivity(intent);
-            }
-        });
     }
 }
